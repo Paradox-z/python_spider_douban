2ecdd887373035f8c09a9ed081ad9e3752d46a6b
Shujun Yuan <66310726+Paradox-z@users.noreply.github.com>
2023-10-27 10:49:21 +0800

    Update get_movie_data.py
    Add some annotations to fit the code more readability.

--- get_movie_data.py
+++ get_movie_data.py
@@ -44,9 +44,6 @@ moviedata = ' [' \
 
 def get_url_data_in_ranking_list(typeId, movie_count, rating, vote_count):
     """
-    从排行榜中获取电影数据
-    typeId 电影类型, movie_count 获取的该电影类型的数量, rating 电影的评分, vote_count 电影的评价人数
-
     :param typeId:
     :param movie_count:
     :param rating:
@@ -55,16 +52,16 @@ def get_url_data_in_ranking_list(typeId, movie_count, rating, vote_count):
     """
 
     try:
-        context = _create_unverified_context()  # 屏蔽ssl证书
+        context = _create_unverified_context()  # blocking SSL
         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'}
         url = 'https://movie.douban.com/j/chart/top_list?type=' + str(typeId) + '&interval_id=100:90&action=unwatched&start=0&limit=' + str(movie_count)
         req = urllib.request.Request(url=url, headers=headers)
         f = urllib.request.urlopen(req, context=context)
         response = f.read()
-        jsondata = loads(response)  # 将json转为python对象
+        jsondata = loads(response)  # transforming json to Python objects
 
         res_list = []
-        for subdata in jsondata:  # 依次对每部电影进行操作
+        for subdata in jsondata:  # operating on each film in turn
             if (float(subdata['rating'][0]) >= float(rating)) and (float(subdata['vote_count']) >= float(vote_count)):
                 sub_list= []
                 sub_list.append(subdata['title'])
@@ -84,26 +81,25 @@ def get_url_data_in_ranking_list(typeId, movie_count, rating, vote_count):
 
 def get_url_data_in_keyword(key_word):
     """
-    从关键字获取电影数据
     :param key_word:
     :return:
     """
 
     chrome_options = Options()
-    chrome_options.add_argument('--headless')  # 设置为无头模式，即不显示浏览器
-    chrome_options.add_argument('user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36"')  # 设置user=agent
-    chrome_options.add_experimental_option('excludeSwitches', ['enable-automation'])  # 此步骤很重要，设置为开发者模式，防止被各大网站识别出来使用了Selenium
-    chrome_options.add_experimental_option("prefs", {"profile.managed_default_content_settings.images": 2})  # 不加载图片,加快访问速度
+    chrome_options.add_argument('--headless')  # Setting the headless mode
+    chrome_options.add_argument('user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36"')  # set user=agent
+    chrome_options.add_experimental_option('excludeSwitches', ['enable-automation'])  # Setting up Developer Mode
+    chrome_options.add_experimental_option("prefs", {"profile.managed_default_content_settings.images": 2})  # do not load images
 
-    load_driver_success = False  # 加载chromedriver驱动是否成功
+    load_driver_success = False  # Loading ChromeDriver
     browser = None
     wait = None
     try:
-        browser = webdriver.Chrome(executable_path='D:/Program Files (x86)/chromedriver.exe', chrome_options=chrome_options)  # 设置chromedriver驱动路径
-        browser.set_page_load_timeout(10)  # 页面加载超时时间为10s
-        browser.set_script_timeout(10)  # 页面js加载超时时间为10s
+        browser = webdriver.Chrome(executable_path='D:/Program Files (x86)/chromedriver.exe', chrome_options=chrome_options)  # set ChromeDriver path
+        browser.set_page_load_timeout(10)  # webpage loading timeout duration sets 10s
+        browser.set_script_timeout(10)  # webpage js loading timeout duration sets 10s
 
-        wait = WebDriverWait(browser, 10)  # 等待超时时长为10s
+        wait = WebDriverWait(browser, 10)  # waiting timeout duration sets 10s
         load_driver_success = True
     except Exception as ex:
         load_driver_success = False
@@ -111,54 +107,54 @@ def get_url_data_in_keyword(key_word):
         return [err_str]
 
 
-    # 加载chromedriver驱动成功时执行的操作
+    # ChromeDriver running well
     if load_driver_success:
 
         try:
-            # 浏览网页
-            browser.get('https://movie.douban.com/subject_search?search_text=' + urllib.parse.quote(key_word) + '&cat=1002')  # get方式获取返回数据
-            # js动态渲染的网页,必须等到搜索结果元素(DIV中class=root)出来后，才可以停止加载网页
-            # 等待DIV中class=root的元素出现
+            # browsing papes
+            browser.get('https://movie.douban.com/subject_search?search_text=' + urllib.parse.quote(key_word) + '&cat=1002')  # get methon, to capture return data
+            # js dynamically rendered web pages
+            # waiting class=root element in div
             wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '.root')))
 
-            dr = browser.find_elements(by=By.XPATH, value="//div[@class='item-root']") # 获取class为item-root的DIV(因为有多个结果)
+            dr = browser.find_elements(by=By.XPATH, value="//div[@class='item-root']") # Cause there are multiple results, get div with class item-root
             jsondata = []
             res_list = []
             for son in dr:
                 moviedata = {'rating': ['', 'null'], 'cover_url': '', 'types': '', 'title': '', 'url': '', 'release_date': '', 'vote_count': '', 'actors': ''}
                 sub_list = ['', '', '', '']
 
-                url_element = son.find_elements(by=By.XPATH, value=".//a")  # 获取第一个a标签的url(因为有多个结果)
+                url_element = son.find_elements(by=By.XPATH, value=".//a")  # Cause there are multiple results, get the url of the first 'a' tag
                 if url_element:
                     moviedata['url'] = (url_element[0].get_attribute("href"))
 
-                img_url_element = url_element[0].find_elements(by=By.XPATH, value=".//img")  # 获取影片海报图片地址
+                img_url_element = url_element[0].find_elements(by=By.XPATH, value=".//img")  # get the film poster image address
                 if img_url_element:
                     moviedata['cover_url'] = (img_url_element[0].get_attribute("src"))
 
-                title_element = son.find_elements(by=By.XPATH, value=".//div[@class='title']")  # 获取标题
+                title_element = son.find_elements(by=By.XPATH, value=".//div[@class='title']")  # get title element
                 if title_element:
                     temp_title = title_element[0].text
                     moviedata['title'] = (temp_title.split('('))[0]
                     moviedata['release_date'] = temp_title[temp_title.find('(') + 1:temp_title.find(')')]
                     sub_list[0] = moviedata['title']
 
-                rating_element = son.find_elements(by=By.XPATH, value=".//span[@class='rating_nums']")  # 获取评分
+                rating_element = son.find_elements(by=By.XPATH, value=".//span[@class='rating_nums']")  # get ranking
                 if rating_element:
                     moviedata['rating'][0] = rating_element[0].text
                     sub_list[1] = moviedata['rating'][0]
 
-                vote_element = son.find_elements(by=By.XPATH, value=".//span[@class='pl']")  # 获取数量
+                vote_element = son.find_elements(by=By.XPATH, value=".//span[@class='pl']")  # get amount
                 if vote_element:
                     moviedata['vote_count'] = vote_element[0].text.replace('(', '').replace(')', '').replace('人评价', '')
                     sub_list[3] = moviedata['vote_count']
 
-                type_element = son.find_elements(by=By.XPATH, value=".//div[@class='meta abstract']")  # 获取类型
+                type_element = son.find_elements(by=By.XPATH, value=".//div[@class='meta abstract']")  # get type element
                 if type_element:
                     moviedata['types'] = type_element[0].text
                     sub_list[2] = moviedata['types']
 
-                actors_element = son.find_elements(by=By.XPATH, value=".//div[@class='meta abstract_2']")  # 获取演员
+                actors_element = son.find_elements(by=By.XPATH, value=".//div[@class='meta abstract_2']")  # get actor element
                 if actors_element:
                     moviedata['actors'] = actors_element[0].text
 
@@ -172,6 +168,6 @@ def get_url_data_in_keyword(key_word):
             return [res_list, jsondata]
 
         except Exception as ex:
-            browser.quit()  # 关闭浏览器
+            browser.quit()  # browser closed
             err_str = "chromedriver驱动加载成功，但是出现其他未知异常：{}".format(ex)
             return [err_str]
